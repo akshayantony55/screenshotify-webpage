101267f7324c3122dccf20ded801d8d0436a8127
Akshay Antony <akshaya@valorem.com>
2020-11-03 23:36:11 +0530

    unhandled promise rejection error in production

--- server.js
+++ server.js
@@ -9,38 +9,42 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 // API calls
 app.get('/api/hello', (req, res) => {
-  res.send({ express: 'Hello From Express' });
+    res.send({ express: 'Hello From Express' });
 });
 app.post('/api/world', (req, res) => {
-  console.log(req.body);
-  res.send(
-    `I received your POST request. This is what you sent me: ${req.body.post}`,
-  );
+    console.log(req.body);
+    res.send(
+        `I received your POST request. This is what you sent me: ${req.body.post}`,
+    );
 });
 
-app.get('/api/screenshot', async (req, res) => {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    const url = req.query.url;
-    await page.goto(url);
-    const screenshotBuffer = await page.screenshot({encoding: "base64" });
+app.get('/api/screenshot', async (req, res, next) => {
+    try {
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+        const url = req.query.url;
+        await page.goto(url);
+        const screenshotBuffer = await page.screenshot({ encoding: "base64" });
 
-    // Respond with the image
-    res.writeHead(200, {
-        'Content-Type': 'image/png',
-        'Content-Length': screenshotBuffer.length
-    });
-    res.end(screenshotBuffer);
+        // Respond with the image
+        res.writeHead(200, {
+            'Content-Type': 'image/png',
+            'Content-Length': screenshotBuffer.length
+        });
+        res.end(screenshotBuffer);
 
-    await browser.close();
+        await browser.close();
+    } catch (err) {
+        next(err);
+    }
 })
 
 if (process.env.NODE_ENV === 'production') {
-  // Serve any static files
-  app.use(express.static(path.join(__dirname, 'client/build')));
-  // Handle React routing, return all requests to React app
-  app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-  });
+    // Serve any static files
+    app.use(express.static(path.join(__dirname, 'client/build')));
+    // Handle React routing, return all requests to React app
+    app.get('*', function (req, res) {
+        res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    });
 }
 app.listen(port, () => console.log(`Listening on port ${port}`));
